da1ae1509d50f16b63c56a89c4fb0c7d5c615ce6
LikhithaKonkimalla <52676162+LikhithaKonkimalla@users.noreply.github.com>
2019-07-09 00:44:08 +0530

    Add files via upload

---
+++ PrintOddLetters/PrintOddLetters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintOddLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j,k;
+            string input = "program";
+            int len = input.Length;
+            string temp = input;
+            for(i=0;i<len;i++)
+            {
+                j=len-1-i;
+                for (k = 0; k < len; k++)
+                {
+                    if (i == k || j == k)
+                    {
+                        Console.Write(input[k]);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
